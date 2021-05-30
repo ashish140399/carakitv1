ef0c515e41dbb064bf7f3a97c5bbf3e7c608162c
deazyyy <d.s@vimerco.com>
2021-05-30 09:14:17 +0530

    initial

---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+/** @jsx jsx */
+import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <Box as="footer" variant="footer">
+      Copyright &copy; {new Date().getFullYear()}. All rights reserved.
+    </Box>
+  )
+}
+
+export default Footer
